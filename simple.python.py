67f39cbd8bce34664b23ee0e2a5b0f5095644a82
Abhay1610 <abhay.k@datalabscorp.com>
2023-10-06 06:14:47 -0700

    this is my file

---
+++ simple.python.py
@@ -0,0 +1,1 @@
+print("modification of existing file")
--- simple_python.py
+++ simple_python.py
@@ -1,3 +1,6 @@
 print("trying to understand git basics")
 print("simple change to existing file")
 print("how are you")
+print("Alex")
+print("vikram")
+
